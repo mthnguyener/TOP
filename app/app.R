c7910ba93333bcb932c243479ab110755f350d92
Chace Paulson <chacepaulson@Chaces-MacBook-Air-2.local>
2020-11-05 18:44:32 -0500

    shiny update

--- app/app.R
+++ app/app.R
@@ -1,21 +1,30 @@
 # Load Packages -----------------------------------------------------------
 library(shiny)
+library(tidyverse)
 
 # Load Data ---------------------------------------------------------------
-
+hourly <- read_csv("hourly.csv")
+traffic.flow <- read_csv("traffic_flow.csv")
 
 # User Interface ----------------------------------------------------------
 
 ui <- fluidPage(
-    
+  titlePanel("Assessment of Air Quality and Traffic Volume"),
+  h4("Washington, D.C."),
+  
+  tabsetPanel(
+    tabPanel("Citywide"),
+    tabPanel("Location Search")
+  )
 )
+?titlePanel
 
 # Server ------------------------------------------------------------------
 
 
 server <- function(input, output) {
-    
-    
+  
+  
 }
 
 
